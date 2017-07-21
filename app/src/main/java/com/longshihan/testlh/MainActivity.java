9f33900698649395c5d419b2f66e2254927edbee
longshihan1 <577093937@qq.com>
2017-07-21 18:33:32 +0800

    添加数据监听

--- app/src/main/java/com/longshihan/testlh/MainActivity.java
+++ app/src/main/java/com/longshihan/testlh/MainActivity.java
@@ -2,6 +2,8 @@ package com.longshihan.testlh;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.longshihan.lh.ui.StatusLayout;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
         StatusLayout layout;
+        View view = LayoutInflater.from(this).inflate(R.layout.emptylayout, null);
         //layout.setLayoutConfig();
        /* View view = LayoutInflater.from(this).inflate(R.layout.emptylayout, null);
         mStatusLayoutManager = StatusLayoutManager.newBuilder(this).setEmptyview(view).build();
---
+++ lh/src/main/java/com/longshihan/lh/ui/SetLoadDataListener.java
@@ -0,0 +1,13 @@
+package com.longshihan.lh.ui;
+
+/**
+ * @author longshihan
+ * @time 2017/7/21 18:23
+ * @des
+ */
+
+public interface SetLoadDataListener {
+    void onLoadingClick();
+    void onErrorClick();
+    void onEmptyClick();
+}
